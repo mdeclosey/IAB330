04b66339b8066eb5010297b5b5f7862c05cbf5cf
Kiara Davison <kiara@davisons.net>
2017-09-25 12:30:56 +1000

    Fix Project and Test

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IAB330assignment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            passwordEntry.IsPassword = true;
+        }
+    }
+}
